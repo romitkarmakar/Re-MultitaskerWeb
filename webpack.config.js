afd2ede0dc6391dd6aa00d0f0e26d8c51a8ed781
theseregrets <rishitashaw49@gmail.com>
2021-12-10 17:26:28 +0530

    tried bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    open: true,
+    host: "localhost"
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader"
       },
-    output: {
-        path: path.resolve(__dirname, './public'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, './public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/i,
-                use: [ "style-loader", "css-loader" ],
-            },
-            {
-                test: /\.(png|jp(e*)g|svg|gif)$/,
-                use: [ 'file-loader' ],
-            },
-            {
-                test: /\.svg$/,
-                use: [ '@svgr/webpack' ],
-            },
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin( {
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        } ),
-    ],
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset"
+      }
 
-}
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ]
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
