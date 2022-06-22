fb0c96f08e885f5ce5dd3c9bf1f2e8b18689240f
romitkarmakar <romitkarmakar@gmail.com>
2022-06-22 10:59:49 +0530

    Fixed category page

--- src/components/categrory/category.jsx
+++ src/components/categrory/category.jsx
@@ -1,199 +1,121 @@
-import React from 'react'
-import "./category.css"
-import ScrollReveal from 'scrollreveal'
+import React from "react";
+import "./category.css";
+import ScrollReveal from "scrollreveal";
 
-export default function Category () {
-  ScrollReveal().reveal( '.box' )
+const categories = [
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3899/3899147.png",
+    name: "Cleaning",
+    description:
+      "Cleaning is the process of removing dirt, dust, or other material from a surface.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1949/1949291.png",
+    name: "Trucking",
+    description: "Trucking is the transportation of goods by trucks.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/2767/2767679.png",
+    name: "Handyman",
+    description:
+      "Handyman is the professional service of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1518/1518965.png",
+    name: "Landscaper",
+    description:
+      "Landscaper is the professional service of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1995/1995507.png",
+    name: "Plumbing",
+    description:
+      "Plumbing is the process of installing and maintaining plumbing equipment.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3400/3400272.png",
+    name: "Lifestyle",
+    description:
+      "Lifestyle is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/2806/2806972.png",
+    name: "Graphic/design",
+    description:
+      "Graphic/design is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1320/1320477.png",
+    name: "Security",
+    description:
+      "Security is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3078/3078996.png",
+    name: "Event Planning",
+    description:
+      "Event Planning is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1085/1085850.png",
+    name: "Transport",
+    description:
+      "Transport is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3069/3069343.png",
+    name: "Catering",
+    description:
+      "Catering is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3081/3081288.png",
+    name: "Run Errand",
+    description:
+      "Run Errand is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/2907/2907253.png",
+    name: "Music",
+    description: "Music is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/3209/3209094.png",
+    name: "Sitter",
+    description: "Sitter is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/2919/2919733.png",
+    name: "Section",
+    description: "Section is the process of providing assistance to customers.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/1182/1182761.png",
+    name: "ASAP",
+    description: "ASAP is the process of providing assistance to customers.",
+  },
+];
+
+export default function Category() {
+  ScrollReveal().reveal(".box");
+
   return (
-    <div id="category" className='main-box'>
+    <div id="category" className="main-box">
       <div class="container">
         <div class="row pt-4">
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3899/3899147.png" alt="" />
-                <div class="box__title">
-                  <h6>Home Cleaning</h6>
-                  <p>6 Listings</p>
-                </div>
-              </a>
+          {categories.map((category) => (
+            <div class=" col-lg-3 col-sm-6">
+              <div class="box mb-3 m-2 p-4">
+                <a href="#">
+                  <img src={category.image} alt="" />
+                  <h6 className="category-title">{category.name}</h6>
+                  <div className="category-description">{category.description}</div>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/1949/1949291.png" alt="" />
-                <div class="box__title">
-                  <h6>Trucking</h6>
-                  <p>3 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/2767/2767679.png" alt="" />
-                <div class="box__title">
-                  <h6>Handyman</h6>
-                  <p>2 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/1518/1518965.png" alt="" />
-                <div class="box__title">
-                  <h6>Landscaper</h6>
-                  <p>7 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/1182/1182761.png" alt="" />
-                <div class="box__title">
-                  <h6>ASAP</h6>
-                  <p>1 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3400/3400272.png" alt="" />
-                <div class="box__title">
-                  <h6>Lifestyle</h6>
-                  <p>8 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/2806/2806972.png" alt="" />
-                <div class="box__title">
-                  <h6>Graphic/design</h6>
-                  <p>12 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/2919/2919733.png" alt="" />
-                <div class="box__title">
-                  <h6>Section</h6>
-                  <p>6 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3209/3209094.png" alt="" />
-                <div class="box__title">
-                  <h6>Sitter</h6>
-                  <p>3 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/2907/2907253.png" alt="" />
-                <div class="box__title">
-                  <h6>Music</h6>
-                  <p>2 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3081/3081288.png" alt="" />
-                <div class="box__title">
-                  <h6>Run Errand</h6>
-                  <p>7 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3069/3069343.png" alt="" />
-                <div class="box__title">
-                  <h6>Catering</h6>
-                  <p>1 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/1085/1085850.png" alt="" />
-                <div class="box__title">
-                  <h6>Transport</h6>
-                  <p>8 photos</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/3078/3078996.png" alt="" />
-                <div class="box__title">
-                  <h6>Event Planning</h6>
-                  <p>12 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-
-          <div class=" col-lg-3 col-sm-6">
-            <div class="box mb-3 m-2">
-              <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/1320/1320477.png" alt="" />
-                <div class="box__title">
-                  <h6>Security</h6>
-                  <p>6 Listings</p>
-                </div>
-              </a>
-            </div>
-          </div>
-
+          ))}
         </div>
-
       </div>
     </div>
-
-  )
+  );
 }
